9aa721d774ff8d62a519d2b50e24f4e82e434845
hasantalukder1234 <hasantalukdercou@gmail.com>
2023-10-24 12:28:35 +0600

    Deleted User from Database

--- src/components/User.js
+++ src/components/User.js
@@ -5,12 +5,27 @@ const User = () => {
     
     const users = useLoaderData();
 
+    const handleDelete = _id => {
+         console.log(_id);
+         fetch(`http://localhost:4000/users/${_id}`, {
+            method:'DELETE',
+         })
+         .then(res => res.json())
+         .then(data => {
+            console.log(data);
+            if(data.deletedCount > 0 ){
+                alert('Deleted User successfully');
+            }
+         })
+    }
+
     return (
         <div>
             <h3>user: {users.length}</h3>
             <div>
                 {
-                    users.map(user => <p key={user._id}> {user.name} {user.email}</p>)
+                    users.map(user => <p key={user._id}> {user.name} {user.email} <button
+                     onClick={()=> handleDelete(user._id)}>X</button></p>)
                 }
             </div>
         </div>
